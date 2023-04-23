e2d3b9facc24250f3bbbe7f11a27ee97e34b7293
badorius <salvadormuntane@gmail.com>
2023-04-23 11:58:33 +0200

    Add rect

--- sounds.py
+++ sounds.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.mixer.init()
-#pygame.mixer.music.load('assets/music/background/2020-02-04_-_Powerful_-_David_Fesliyan.mp3')
-#pygame.mixer.music.play()
+pygame.mixer.music.load('assets/music/background/2020-02-04_-_Powerful_-_David_Fesliyan.mp3')
+pygame.mixer.music.play()
 ouch = pygame.mixer.Sound('assets/music/fast-simple-chop-5-6270.mp3')
 hurt = pygame.mixer.Sound('assets/music/female_hurt-87490.mp3')
--- world.py
+++ world.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from random import randint
 
 class World():
     def __init__(self, data):
@@ -9,6 +10,7 @@ class World():
         #load img
         dirt_img = pygame.image.load('assets/Tiles/floor_tile_2.png')
         grass_img = pygame.image.load('assets/Tiles/floor_tile_carpet_2.png')
+        barrel_img = pygame.image.load('assets/Decorations/barrel.png')
 
         row_count = 0
         for row in data:
@@ -17,14 +19,16 @@ class World():
                 if tile == 1:
                     img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
-                    img_rect.x = (col_count * tile_size) - (self.scroll * self.speed)
+
+                    img_rect.x = col_count * tile_size
+                    print(img_rect.x)
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
                 if tile == 2:
-                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img = pygame.transform.scale(barrel_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size - (self.scroll * self.speed)
+                    img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
@@ -36,6 +40,15 @@ class World():
         speed = 1
         for tile in self.tile_list:
             win.blit(tile[0], tile[1])
+
+    def draw_ground(self):
+        floor_rnd = randint(1, 10)
+        for z in range(1000):
+
+            for x in range(1, 5):
+                win.blit(self.floor1[x - 1], (
+                x + z * (self.floor1[x - 1].get_width()) - self.scroll * 2.2, window_height - self.floor1[x - 1].get_height()))
+
 
     def drawgrid(self):
         for line in range(0, 400):
