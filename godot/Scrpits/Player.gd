140bd0089b1419572b3b6b51915bb581ae9a5a7e
badorius <salvadormuntane@gmail.com>
2023-11-20 21:43:50 +0100

    Add bowfire and ui_fire map

--- godot/Scrpits/Player.gd
+++ godot/Scrpits/Player.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var score : int = 0
-@onready var score_text : Label = get_node("CanvasLayer/ScoreText")
+@onready var score_text : Label = get_node("../CanvasLayer/ScoreText")
 
 
 func _physics_process(delta):
@@ -19,22 +19,27 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		if direction == -1:
-			get_node( "Warrior" ).set_flip_h( false )
-			velocity.x = direction * SPEED
-			$AnimationPlayer.play("WalkRight")
-		if direction == 1:
-			velocity.x = direction * SPEED
-			get_node( "Warrior" ).set_flip_h( true )
-			$AnimationPlayer.play("WalkRight")
-			
-	else:
-		$AnimationPlayer.stop()
+	if Input.is_action_pressed("ui_fire1"):
+		$AnimationPlayer.play("BowShooting")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	else:
+		if direction:
+			if direction == -1:
+				get_node( "Warrior" ).set_flip_h( false )
+				velocity.x = direction * SPEED
+				$AnimationPlayer.play("WalkRight")
+			if direction == 1:
+				velocity.x = direction * SPEED
+				get_node( "Warrior" ).set_flip_h( true )
+				$AnimationPlayer.play("WalkRight")
+				
+		else:
+			$AnimationPlayer.stop()
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	
